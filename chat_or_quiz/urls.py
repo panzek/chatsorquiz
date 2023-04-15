5d392e9a7b262459ab0f8c6f4af43161e89fd354
Peter Anny-Nzekwue <panzek@yahoo.co.uk>
2023-04-15 14:46:45 +0100

    Create url and template to render the view

--- chat_or_quiz/urls.py
+++ chat_or_quiz/urls.py
@@ -21,5 +21,6 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include('allauth.urls')),
+    path('feedback/', include('feedback.urls')),
     path('', include('home.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.STATIC_ROOT)
