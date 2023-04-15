15d23b919265e533e9354829dc7aab107d03ec05
Peter Anny-Nzekwue <panzek@yahoo.co.uk>
2023-04-15 13:36:49 +0100

    Create a view to render feedback form in template

--- feedback/views.py
+++ feedback/views.py
@@ -1,3 +1,23 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .forms import FeedbackForm
 
-# Create your views here.
+
+def feedback(request):
+
+    """ A view to render feedback form in template """
+    
+    if request.method == 'POST':
+        form = FeedbackForm(request.POST)
+
+        if form.is_valid:
+            form.save()
+            return redirect('home')
+    
+    form = FeedbackForm()
+
+    template = 'feedback/feedback.html'
+    context = {
+        'form': form,
+    }
+
+    return render(request, template, context)
